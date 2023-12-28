0ea1f8523e919d9e4314909a32443571891c8309
freeram <kulikovpavel2801@gmail.com>
2023-12-27 21:29:13 -0700

    Add string month parsing, validation

--- discordbot/cogs/googlecalendar.py
+++ discordbot/cogs/googlecalendar.py
@@ -54,9 +54,9 @@ class EventDateModal(discord.ui.Modal, title="Set The Date"):
 
     month = discord.ui.TextInput(
         label="Enter month (1-12 or name)",
-        default=datetime.now().month,
+        default=datetime.now().strftime("%B"),
         min_length=1,
-        max_length=15
+        max_length=9
     )
 
     year = discord.ui.TextInput(
@@ -89,7 +89,7 @@ class EventCreationView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=3600)
         self.name_modal = None
-        self.time_modal = None
+        self.date_modal = None
         self.eventinfo = None
 
     @discord.ui.button(label="Set Title", emoji="✏️", style=discord.ButtonStyle.primary)
@@ -106,24 +106,27 @@ class EventCreationView(discord.ui.View):
 
     @discord.ui.button(label="Set Date", emoji="📅", style=discord.ButtonStyle.primary)
     async def set_date(self, interaction: discord.Interaction, button: discord.ui.Button):
+        # On click, create the modal and send it to the user
         modal = EventDateModal()
         await interaction.response.send_modal(modal)
+        # Wait until user submits info
         await modal.wait()
-        self.time_modal = modal
+        self.date_modal = modal
+        # Make it gray (clicked)
         button.style = discord.ButtonStyle.gray
         await self.check_readiness(interaction)
 
     async def check_readiness(self, interaction: discord.Interaction):
-        name_modal, time_modal = self.name_modal, self.time_modal
+        name_modal, date_modal = self.name_modal, self.date_modal
         # Submit button disabled by default
         submit_button = self.children[2]
         submit_button.disabled = True
         submit_button.style = discord.ButtonStyle.grey
 
-        if time_modal:
+        if date_modal:
             error_payload = ""
-            day, month, year = time_modal.day.value, time_modal.month.value, time_modal.year.value
-            time, duration = time_modal.time.value, time_modal.duration.value
+            day, month, year = date_modal.day.value, date_modal.month.value, date_modal.year.value
+            time, duration = date_modal.time.value, date_modal.duration.value
 
             if not self.return_valid_date(day, month, year):
                 error_payload += "**Error:** invalid day, month or year entered"
@@ -137,7 +140,7 @@ class EventCreationView(discord.ui.View):
                 await interaction.edit_original_response(view=self, content=error_payload)
                 return
 
-        if not name_modal or not time_modal:
+        if not name_modal or not date_modal:
             # Either the title or time modal not submitted, return
             await interaction.edit_original_response(view=self)
             return
@@ -148,9 +151,22 @@ class EventCreationView(discord.ui.View):
         await interaction.edit_original_response(view=self, content="**Ready to add event to calendar**")
 
     def return_valid_date(self, day, month, year):
+        # Dictionary to map month names to numbers
+        month_mapping = {
+            'january': 1, 'february': 2, 'march': 3, 'april': 4, 'may': 5, 'june': 6,
+            'july': 7, 'august': 8, 'september': 9, 'october': 10, 'november': 11, 'december': 12
+        }
+
         try:
+            # Convert month name to number if necessary
+            if isinstance(month, str) and not month.isdigit():
+                month = month.lower()
+                month = month_mapping.get(month, -1)  # Month not found = return -1, will cause ValueError in datetime
+
+            # Create date object
             date = datetime(int(year), int(month), int(day))
             return date
+
         except ValueError:
             return False
 
@@ -180,22 +196,33 @@ class EventCreationView(discord.ui.View):
 
     @discord.ui.button(label="Submit", emoji="✅", style=discord.ButtonStyle.grey, disabled=True)
     async def submit(self, interaction: discord.Interaction, button: discord.ui.Button):
-        name_modal, time_modal = self.name_modal, self.time_modal
+        name_modal, date_modal = self.name_modal, self.date_modal
 
-        day, month, year = time_modal.day.value, time_modal.month.value, time_modal.year.value
-        time, duration = time_modal.time.value, time_modal.duration.value
+        # Extract values from modals
+        day, month, year = date_modal.day.value, date_modal.month.value, date_modal.year.value
+        time, duration = date_modal.time.value, date_modal.duration.value
         summary, description, location = name_modal.summary.value, name_modal.description.value, name_modal.location.value
 
-        start_time = self.return_valid_time(time).replace(
-            day=int(day), month=int(month), year=int(year))
+        # Use return_valid_date to get the date
+        start_date = self.return_valid_date(day, month, year)
+        if not start_date:
+            print("Submit failed in googlecalendar.py, EventCreationView: invalid date")
+            return
+
+        # Use return_valid_time to parse the time (assuming this function exists)
+        start_time = self.return_valid_time(time)
+
+        # Combine date and time for start_time
+        start_time = start_time.replace(year=start_date.year, month=start_date.month, day=start_date.day)
+
+        # Calculate duration and end_time
         duration_minutes = self.return_valid_duration(duration)
-        end_time = (start_time + timedelta(minutes=duration_minutes)
-                    ).replace(day=int(day), month=int(month), year=int(year))
+        end_time = start_time + timedelta(minutes=duration_minutes)
 
-        # !plancli args: Summary,Location,Description,Start time,End time
-        self.eventinfo = [summary, location, description,
-                            start_time.isoformat(), end_time.isoformat()]
+        # Prepare event information
+        self.eventinfo = [summary, location, description, start_time.isoformat(), end_time.isoformat()]
 
+        # Clear items, respond to interaction, and stop
         self.clear_items()
         await interaction.response.defer()
         await interaction.delete_original_response()
