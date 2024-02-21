b84a8c344ed2a327a8e896fa7460ab4ca3438e06
freeram <kulikovpavel2801@gmail.com>
2024-02-21 06:15:14 -0700

    Condense get_remindable_events and get_events into one function

--- discordbot/api/google_calendar_api.py
+++ discordbot/api/google_calendar_api.py
@@ -1,7 +1,6 @@
 import json
 import os
-from datetime import datetime
-
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -47,51 +46,37 @@ class GoogleCalendarAPI:
             print(f"Google calendar initialization failed: {error}")
             return None
 
-    def get_events(self, max_results=10):
+    def get_events(self, max_results=10, custom_reminders=False):
         try:
             now = datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+            query_params = {
+                "calendarId": "primary",
+                "timeMin": now,
+                "singleEvents": True,
+                "orderBy": "startTime",
+            }
 
-            # Getting the upcoming max_results events
-            events_result = (
-                self.service.events()
-                .list(
-                    calendarId="primary",
-                    timeMin=now,
-                    maxResults=max_results,
-                    singleEvents=True,
-                    orderBy="startTime"
-                )
-                .execute()
-            )
+            if max_results is not None:
+                query_params["maxResults"] = max_results
+
+            if custom_reminders is True:
+                # Currently, the earliest custom reminder for an event is 1 week
+                # So when googlecalendar.py calls this to find events that need a reminder scheduled
+                # We do not care about anything past that point
+                one_week_from_now = datetime.utcnow() + timedelta(weeks=1, hours=1)
+                one_week_from_now_iso = one_week_from_now.isoformat() + "Z"
+                query_params["timeMax"] = one_week_from_now_iso
+
+                custom_reminders_val = "true" if custom_reminders else "false"
+                query_params["privateExtendedProperty"] = f"custom={custom_reminders_val}"
+
+            events_result = self.service.events().list(**query_params).execute()
             events = events_result.get("items", [])
 
             return events
 
         except HttpError as error:
-            return f"An error occured: {error}"
-
-    def get_remindable_events(self):
-        try:
-            now = datetime.utcnow().isoformat() + "Z"
-
-            # Getting all remindable events
-            events_result = (
-                self.service.events()
-                .list(
-                    calendarId="primary",
-                    timeMin=now,
-                    singleEvents=True,
-                    orderBy="startTime",
-                    privateExtendedProperty="remindable=true"
-                )
-                .execute()
-            )
-            events = events_result.get("items", [])
-
-            return events
-
-        except HttpError as error:
-            return f"An error occured: {error}"
+            return f"An error occurred: {error}"
 
     def create_event(self, summary, location, description, start_time: datetime, end_time: datetime, roles: list = None, mins_before_reminder: list = None):
         # Example arguments:
@@ -103,7 +88,7 @@ class GoogleCalendarAPI:
         # Convert additional data to json
         roles_json = json.dumps(roles) if roles is not None else ''
         minutes_list_json = json.dumps(mins_before_reminder) if mins_before_reminder is not None else ''
-        remindable = "true" if mins_before_reminder is not None else 'false'
+        is_custom = "true" if mins_before_reminder is not None else 'false'
 
         # mins_before_reminder is a list of integers which represent minutes
         # mins_before_reminder = [60, 30, 15] means: remind about this event 60 minutes before it happens, 30, 15 minutes.
@@ -131,7 +116,7 @@ class GoogleCalendarAPI:
                 'private': {
                     'roles': roles_json,  # Roles to remind
                     'reminderMinutes': minutes_list_json,
-                    'remindable': remindable
+                    'custom': is_custom
                 }
             }
         }
