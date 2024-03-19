f517480408f3635be579a3abe8435b95b5b5d574
Pavel Kulikov <138463896+freeram@users.noreply.github.com>
2024-03-19 14:13:26 -0600

    Fix bug

--- discordbot/main.py
+++ discordbot/main.py
@@ -42,7 +42,7 @@ async def load_extensions():
     await bot.load_extension(f"cogs.googlecalendar")
     await bot.load_extension(f"cogs.channels")
     await bot.load_extension(f"cogs.names")
-await bot.load_extension(f"cogs.uai")
+    await bot.load_extension(f"cogs.uai")
 
 
 async def main():
